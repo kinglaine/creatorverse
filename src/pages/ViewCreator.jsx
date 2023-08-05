f54554834b976584427525f8c48484cc8709d33b
woodley <gelinw3@gmail.com>
2023-08-05 17:37:54 -0400

    style: add style to view single creator

--- src/pages/ViewCreator.jsx
+++ src/pages/ViewCreator.jsx
@@ -1,13 +1,18 @@
 // page to view a single content creator
 import React, { useEffect, useState } from 'react'
 import {supabase} from '../client'
+import { useNavigate } from "react-router-dom";
+import { faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function ViewCreator() {
   const url = window.location.href; // Get the current URL
   const segments = url.split('/'); // Split the URL by '/'
   const creatorId = segments[segments.length - 1];
+
   console.log("id is", creatorId);
   const [creator, setCreator] = useState(null);
+
   const fecthCreatorInfo = async () => {
     if(creatorId){
       let { data: creator, error } = await supabase.from('creators').select().eq('id', creatorId);
@@ -19,12 +24,45 @@ export default function ViewCreator() {
 
   useEffect(() => {
     fecthCreatorInfo();
-  }, [])
+  }, []);
+
+  const navigate = useNavigate();
+  const handleNavigateToEdit = () => {
+    navigate(`/edit/${creatorId}`);
+  };
+  
+  const deleteMatchingRowInCreatorTable = async () => {
+    const { error } = await supabase
+    .from('creators')
+    .delete()
+    .eq('id', creatorId);
+    if(error) {
+      console.log(error);
+    }else{
+      navigate('/');
+      console.log("Deletion was a success");
+    }
+  }
+  const handleDeleteCreator = () => {
+    deleteMatchingRowInCreatorTable();
+  }
+
   return (
-    <div>
-      <p>Hello i'm a creator</p>
-      <p>My name is {creator?.name}</p>
-      <p>Description {creator?.description}</p>
+    <div className="bg-slate-800 border-t-2 border-gray-700 flex flex-col p-10 h-screen">
+      <div className="flex">
+        <img src={creator?.imageURL} alt='This is the creator photo' width="300px"></img>
+        <div className="w-64 ml-5">
+          <p style={{color:'#00a3c4'}}>{creator?.name}</p>
+          <a href={URL} target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon style={{ color: 'white' }} icon={faYoutube} />
+          </a>
+          <p style={{color: "white"}} className="break-words">Description {creator?.description}</p>
+        </div>
+      </div>
+      <div className="flex justify-center space-x-4">
+        <button style={{color: 'white', borderRadius: '10px'}} className="bg-cyan-600 w-64 h-20" onClick={handleNavigateToEdit}>EDIT</button>
+        <button style={{color: 'white', borderRadius: '10px'}} className="w-64 h-20 bg-red-600" onClick={handleDeleteCreator}>DELETE</button>
+      </div>
     </div>
   )
 }
