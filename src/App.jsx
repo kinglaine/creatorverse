362f4fb8e3ce0dfa73f4eeb4ac6bfdc781fb259e
woodley <gelinw3@gmail.com>
2023-08-04 23:33:43 -0400

    feat: at this point user can navigate between pages with react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
 import './index.css'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+import AddCreator from './pages/AddCreator'
+import EditCreator from './pages/EditCreator'
+import ShowCreators from './pages/ShowCreators'
+import ViewCreator from './pages/ViewCreator'
 
 function App() {
 
   return (
-    <div>
-
-    </div>
+    <Router>
+      <ul>
+        <li>
+          <Link to='/'>Go To Show All Creators</Link>
+        </li>
+        <li>
+          <Link to='/edit'>Go To Edit Creators</Link>
+        </li>
+        <li>
+          <Link to='/view'>Go To View Single Creator</Link>
+        </li>
+        <li>
+          <Link to='/add'>Go Add A new Creator</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path='/' element={<ShowCreators></ShowCreators>}></Route>
+        <Route path='/edit' element={<EditCreator></EditCreator>}></Route>
+        <Route path='/view' element={<ViewCreator></ViewCreator>}></Route>
+        <Route path='/add' element={<AddCreator></AddCreator>}></Route>
+      </Routes>
+    </Router>
   )
 }
 
