c158f154a1c434011e61df76e8f7f63279f1e716
woodley <gelinw3@gmail.com>
2023-08-05 11:39:23 -0400

    featL able to add content creator in creator table

--- src/pages/AddCreator.jsx
+++ src/pages/AddCreator.jsx
@@ -1,8 +1,59 @@
 //page to allow the user to add a new content creator
-import React from 'react'
-
+import React, {useState} from 'react'
+import {supabase} from '../client'
 export default function AddCreator() {
+  const [formData, setFormData] = useState({
+    name: '',
+    description: '',
+    url: '',
+    imageUrl: ''
+  });
+
+  const insertRowInCreatorTable = async () => {
+    const { data, error } = await supabase
+    .from('creators')
+    .insert([
+      {name: formData.name, url: formData.url, description: formData.description, imageURL: formData.imageUrl},
+    ])
+    .select();
+    if(error){
+      console.log("An Error occured while inserting")
+    }else{
+      console.log("Insertion was a success", data)
+    }
+
+  }
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value
+    }))
+  }
+   // Handle form submission here
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    insertRowInCreatorTable();
+    console.log('name:', formData.name);
+    console.log('url:', formData.url);
+    console.log('description:', formData.description);
+    console.log('imageUrl:', formData.imageUrl);
+  };
+
   return (
-    <div>AddCreator</div>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Name: </label>
+        <input type='text' name='name' value={formData.name} onChange={handleChange} required></input>
+        <label>Description: </label>
+        <input type= 'text' name='description' value={formData.description} onChange={handleChange} ></input>
+        <label>Url</label>
+        <input type='text' name='url' value={formData.url} onChange={handleChange} ></input>
+        <label>image url</label>
+        <input type='text' name='imageUrl' value={formData.imageUrl} onChange={handleChange} ></input>
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
   )
 }
