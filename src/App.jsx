d8fa53bc3123efa585d95138e2c58a08d3b888ec
woodley <gelinw3@gmail.com>
2023-08-05 15:47:01 -0400

    style: add style for header

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,28 @@ import AddCreator from './pages/AddCreator'
 import EditCreator from './pages/EditCreator'
 import ShowCreators from './pages/ShowCreators'
 import ViewCreator from './pages/ViewCreator'
-
+import backgroundImage from './assets/banner.jpg'
 function App() {
 
+  const headerStyle = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    height: '500px',
+  };
   return (
     <Router>
-      <ul>
-        <li>
-          <Link to='/'>Go To Show All Creators</Link>
-        </li>
-        <li>
-          <Link to='/edit'>Go To Edit Creators</Link>
-        </li>
-        <li>
-          <Link to='/view:creatorId'>Go To View Single Creator</Link>
-        </li>
-        <li>
-          <Link to='/add'>Go Add A new Creator</Link>
-        </li>
-      </ul>
+      <div style={headerStyle}>
+        <h1 style={{color: 'white'}} className="text-5xl text-center mb-24 pt-24">CREATORVERSE</h1>
+        <ul className="flex justify-evenly">
+          <li>
+            <Link style={{color: 'white', borderRadius: '10px'}} className="bg-cyan-600 w-64 h-20 flex justify-center items-center" to='/'>Go To Show All Creators</Link>
+          </li>
+          <li>
+            <Link style={{color: 'white', borderRadius: '10px'}} className="bg-cyan-600 w-64 h-20 flex justify-center items-center"  to='/add'>Go Add A new Creator</Link>
+          </li>
+        </ul>
+      </div>
       <Routes>
         <Route path='/' element={<ShowCreators></ShowCreators>}></Route>
         <Route path='/edit/:creatorId' element={<EditCreator></EditCreator>}></Route>
