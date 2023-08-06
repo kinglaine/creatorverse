35c00287ffac1f1cc0f879b401ca5357fb952100
woodley <gelinw3@gmail.com>
2023-08-05 23:22:41 -0400

    feat: add twitter and instagram input

--- src/pages/AddCreator.jsx
+++ src/pages/AddCreator.jsx
@@ -5,7 +5,9 @@ export default function AddCreator() {
   const [formData, setFormData] = useState({
     name: '',
     description: '',
-    url: '',
+    youtubeUrl: '',
+    twitterUrl: '',
+    instagram: '',
     imageUrl: ''
   });
 
@@ -42,30 +44,40 @@ export default function AddCreator() {
   };
 
   return (
-    <div className="bg-slate-800 border-t-2 border-gray-700 flex flex-col p-10 h-screen">
+    <div className="bg-slate-800 border-t-2 border-gray-700 flex flex-col p-10 h-full">
       <form onSubmit={handleSubmit} className="flex flex-col items-center">
-        <div className="flex flex-col items-center space-y-10">
+        <div className="flex flex-col items-center space-y-5">
           <label>
-            <p style={{color:'white'}}>Name</p>
+            <p style={{color:'white', fontSize:'20px'}}>Name</p>
             <p></p>
             <input className="w-96 h-14 p-1 rounded"  type='text' name='name' value={formData.name || ''} onChange={handleChange} required></input>
           </label>
 
           <label>
-            <p style={{color:'white'}}>Image</p>
+            <p style={{color:'white', fontSize:'20px'}}>Image</p>
             <p style={{color:'white'}} className="break-words w-96 mb-1"><i>Provide a link to an image of your creator. Be sure to include the http://</i></p>
             <input className="w-96 h-14 p-1 rounded" type='text' name='imageURL' value={formData.imageUrl || ''} onChange={handleChange} required></input>
           </label>
           
           <label>
-            <p style={{color:'white'}}>Description</p>
+            <p style={{color:'white', fontSize:'20px'}}>Description</p>
             <p style={{color:'white'}} className="break-words w-96 mb-1"><i>Provide a description of the creator. Who are they? What makes them interesting?</i></p>
             <input className="w-96 h-14 p-1 rounded" type= 'text' name='description' value={formData.description || ''} onChange={handleChange} required></input>
           </label>
-          
+
           <label>
-            <p style={{color:'white'}}>Youtube</p>
-            <input className="w-96 h-14 p-1 rounded" type='text' name='url' value={formData.url || ''} onChange={handleChange} required></input>
+            <h1 style={{color:'#00a3c4'}}>SOCIAL MEDIA LINKS</h1>
+            <p style={{color:'white'}} className="w-96"><i>Provide at least one of the creator's social media links.</i></p>
+            <p style={{color:'white', fontSize:'20px'}}>Youtube</p>
+            <input className="w-96 h-14 p-1 rounded" type='text' name='url' value={formData.youtubeUrl || ''} onChange={handleChange} required></input>
+          </label>
+          <label>
+            <p style={{color:'white', fontSize:'20px'}}>Twitter</p>
+            <input className="w-96 h-14 p-1 rounded" type='text' name='url' value={formData.twitterUrl || ''} onChange={handleChange} required></input>
+          </label>
+          <label>
+            <p style={{color:'white', fontSize:'20px'}}>Instagram</p>
+            <input className="w-96 h-14 p-1 rounded" type='text' name='url' value={formData.instagram || ''} onChange={handleChange} required></input>
           </label>
         </div>
           <button type='submit' style={{color: 'white', borderRadius: '10px'}} className="bg-cyan-600 w-96 h-10 mt-2">SUBMIT</button>
